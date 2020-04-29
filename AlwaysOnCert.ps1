e370753d3799aa3e5e8ec3e571311dbd4e8528d3
Marc Kean <marc@marckean.com>
2020-04-29 17:02:58 +1000

    .

---
+++ AlwaysOnCert.ps1
@@ -0,0 +1,109 @@
+### RUN AS ADMINISTRATOR for local machine
+
+<#
+Divya - this is the documented way of creating the VPN connection on the local Win 10 machine,
+which I had troubles with it, it creates the VPN connection but then I can't see the connection in Windows,
+however you can see it below with PowerShell and you can connect and disconnect from PowerShell
+#>
+
+#region From https://docs.microsoft.com/en-us/azure/virtual-wan/howto-always-on-device-tunnel
+function devicecert {
+    Param(
+        [string]$xmlFilePath,
+        [string]$ProfileName
+    )
+        
+    $a = Test-Path $xmlFilePath
+    echo $a
+        
+    $ProfileXML = Get-Content $xmlFilePath
+        
+    echo $XML
+        
+    $ProfileNameEscaped = $ProfileName -replace ' ', '%20'
+        
+    $Version = 201606090004
+        
+    $ProfileXML = $ProfileXML -replace '<', '&lt;'
+    $ProfileXML = $ProfileXML -replace '>', '&gt;'
+    $ProfileXML = $ProfileXML -replace '"', '&quot;'
+        
+    $nodeCSPURI = './Vendor/MSFT/VPNv2'
+    $namespaceName = "root\cimv2\mdm\dmmap"
+    $className = "MDM_VPNv2_01"
+        
+    $session = New-CimSession
+        
+    try {
+        $newInstance = New-Object Microsoft.Management.Infrastructure.CimInstance $className, $namespaceName
+        $property = [Microsoft.Management.Infrastructure.CimProperty]::Create("ParentID", "$nodeCSPURI", 'String', 'Key')
+        $newInstance.CimInstanceProperties.Add($property)
+        $property = [Microsoft.Management.Infrastructure.CimProperty]::Create("InstanceID", "$ProfileNameEscaped", 'String', 'Key')
+        $newInstance.CimInstanceProperties.Add($property)
+        $property = [Microsoft.Management.Infrastructure.CimProperty]::Create("ProfileXML", "$ProfileXML", 'String', 'Property')
+        $newInstance.CimInstanceProperties.Add($property)
+        
+        $session.CreateInstance($namespaceName, $newInstance)
+        $Message = "Created $ProfileName profile."
+        Write-Host "$Message"
+    }
+    catch [Exception] {
+        $Message = "Unable to create $ProfileName profile: $_"
+        Write-Host "$Message"
+        exit
+    }
+    $Message = "Complete."
+    Write-Host "$Message"
+}
+
+$VPN_Name = 'AzureDeviceTunnel2'
+devicecert .\VPNProfile.xml $VPN_Name
+#endregion
+
+
+
+<#
+Divya - this is my other way of creating the VPN connection on the local Win 10 machine,
+seems to be a whole lot cleaner, and I can see the connection in the Windows networks tray
+#>
+###############################################################################
+# Addition of the VPN connections
+###############################################################################
+# Alternate method
+$VPN_Name = 'AzureDeviceTunnel'
+$VPN_ServerName = 'hub0.dg67i3fr1l94yn9nwcrsv5fue.vpn.azure.com'
+Add-VpnConnection -AuthenticationMethod MachineCertificate -ServerName $VPN_ServerName `
+    -EncryptionLevel Required -Name $VPN_Name -TunnelType Ikev2 -AllUserConnection -SplitTunneling
+
+###############################################################################
+# Connection of the VPN connection
+###############################################################################
+$vpnName = $VPN_Name;
+    $vpn = Get-VpnConnection -Name $vpnName;
+    if($vpn.ConnectionStatus -eq "Disconnected"){
+    rasdial $vpnName;
+    }
+
+###############################################################################
+# Disconnection of the VPN connection
+###############################################################################
+$vpnName = $VPN_Name;
+$vpn = Get-VpnConnection -Name $vpnName;
+
+if($vpn.ConnectionStatus -eq "Connected"){
+  rasdial $vpnName /DISCONNECT;
+}
+
+###############################################################################
+# Removal of the VPN connections
+###############################################################################
+# Remove Always On VPN
+Get-VpnConnection -Name $VPN_Name | Remove-VpnConnection -Force
+
+###############################################################################
+# Check routes
+###############################################################################
+Get-NetRoute -InterfaceAlias $VPN_Name
+Get-NetIPAddress -InterfaceAlias $VPN_Name | fl *
+
+Get-VpnConnection | fl *
--- README.md
+++ README.md
@@ -15,6 +15,53 @@ and remotely into Azure.
 
 ![A picture containing object, clock, drawing, meter Description automatically generated](media/28c6f1cb4e96fb1629e13f531bd8de50.png)
 
+dd
+
+1.  one script is the VPN config XML file which you don't really need (it's the
+    method on our docs site how to create a VPN connection). 
+
+ other script is to create all the self-signed certificates that you need,
+either user based or machine based - if you want to create a user based or
+machine based VPN
+
+ other script is the VPN connection script. Also here is the bit up the top how
+it's on our docs site, but my other method below seems to connect the same,
+neither get a default gateway, but mine gives this, which you don't get with the
+other way documented on our docs site
+
+Azure Portal
+------------
+
+In the Azure portal, after you setup Azure Virtual WAN as well as a Virtual HUB,
+you want to Create a user VPN config as follows:
+
+![](media/777764436ba58217d74bb596245eac22.png)
+
+The PowerShell script can help you to get **base64** representation of the root
+certificate.
+
+\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+
+\# Azure Stuff, do these other steps in Azure when you setup P2S in the Azure
+Virtual WAN Hub
+
+\# This is a simple copy and paste of two pieces of information \| 'PUBLIC
+CERTIFICATE DATA' and 'ROOT CERTIFICATE NAME'
+
+\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+
+\# Copy the Root Base64 to clipboard, paste this Base64 to Azure Virtual WAN, as
+PUBLIC CERTIFICATE DATA when you edit the user VPN configuration
+
+\$RootCertificate = Get-ChildItem -Path \$RootCertStore \| where {\$_.Subject
+-eq "CN=\$CNroot"}
+
+\$([Convert]::ToBase64String(\$RootCertificate.Export('Cert'),
+[System.Base64FormattingOptions]::InsertLineBreaks)) \| clip
+
+Write-Host "The ROOT CERTIFICATE NAME, when you edit the user VPN configuration,
+is \$CNroot"
+
 Always On VPN
 -------------
 
