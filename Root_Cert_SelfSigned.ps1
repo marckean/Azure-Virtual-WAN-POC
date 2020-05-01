1a4b09710b8ceb367645b8283b84a971f832e7fb
Marc Kean <marc@marckean.com>
2020-05-01 12:05:16 +1000

    .

--- Root_Cert_SelfSigned.ps1
+++ Root_Cert_SelfSigned.ps1
@@ -19,7 +19,6 @@ try {
 
     # Check existing self-signed root certificate
     (Get-ChildItem -Path $RootCertStore | where {$_.Subject -eq "CN=$CNroot"})
-    (Get-ChildItem -Path $ClientCertStore | where {$_.Subject -eq "CN=$CNroot"})
 
 }
 catch {
@@ -29,28 +28,6 @@ catch {
         -Subject "CN=$CNroot" -KeyExportPolicy Exportable `
         -HashAlgorithm sha256 -KeyLength 2048 `
         -CertStoreLocation $ClientCertStore -KeyUsageProperty Sign -KeyUsage CertSign
-
-}
-
-###############################################################################
-# Generate a client certificate
-###############################################################################
-
-try {
-
-    # Check existing client certificate
-    (Get-ChildItem -Path $ClientCertStore | where {$_.Subject -eq "CN=$CNclient"})
-
-}
-catch {
-
-    # Generate a client certificate
-    New-SelfSignedCertificate -Type Custom -DnsName $CNclient -KeySpec Signature `
-        -Subject "CN=$CNclient" -KeyExportPolicy Exportable `
-        -HashAlgorithm sha256 -KeyLength 2048 `
-        -CertStoreLocation $ClientCertStore `
-        -Signer $rootcert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
-    #endregion
 
 }
 
@@ -97,36 +74,6 @@ $([Convert]::ToBase64String($RootCertificate.Export('Cert'), [System.Base64Forma
 }
 
 ###############################################################################
-# Export Client Certificate with private key to Documents Folder
-# Distribute this to certificate to all clients that need to connect P2S to Azure Virtual WAN
-###############################################################################
-
-try {
-
-    # Check for existing client Certificate with private key
-    Get-Item -Path "$ENV:USERPROFILE\Documents\$CNclient.pfx"
-
-}
-catch {
-
-    $mypwd = ConvertTo-SecureString -String $ClientPFXCertPassword -Force -AsPlainText
-    $ClientCertificate = Get-ChildItem -Path $ClientCertStore | where {$_.Subject -eq "CN=$CNclient"}
-    $ClientCertificate | Export-PfxCertificate -FilePath "$ENV:USERPROFILE\Documents\$CNclient.pfx" -Password $mypwd
-
-}
-
-###############################################################################
-# Azure Stuff, do these other steps in Azure when you setup P2S in the Azure Virtual WAN Hub
-# This is a simple copy and paste of two pieces of information | 'PUBLIC CERTIFICATE DATA' and 'ROOT CERTIFICATE NAME'
-###############################################################################
-
-# Copy the Root Base64 to clipboard, paste this Base64 to Azure Virtual WAN, as PUBLIC CERTIFICATE DATA when you edit the user VPN configuration
-$RootCertificate = Get-ChildItem -Path $RootCertStore | where {$_.Subject -eq "CN=$CNroot"}
-$([Convert]::ToBase64String($RootCertificate.Export('Cert'), [System.Base64FormattingOptions]::InsertLineBreaks)) | clip
-
-Write-Host "The ROOT CERTIFICATE NAME, when you edit the user VPN configuration, is $CNroot"
-
-###############################################################################
 # Clean Up - if you want to clean up everything from above, remove all certificates created from above
 ###############################################################################
 
@@ -134,7 +81,3 @@ Write-Host "The ROOT CERTIFICATE NAME, when you edit the user VPN configuration,
 (Get-ChildItem -Path $RootCertStore | where {$_.Subject -eq "CN=$CNroot"}) | Remove-Item -Force -ErrorAction SilentlyContinue
 (Get-ChildItem -Path $ClientCertStore | where {$_.Subject -eq "CN=$CNroot"}) | Remove-Item -Force -ErrorAction SilentlyContinue
 Get-Item -Path "$ENV:USERPROFILE\Documents\$CNroot.cer" | Remove-Item -Force -ErrorAction SilentlyContinue
-
-# Delete the Client Certificate
-(Get-ChildItem -Path $ClientCertStore | where {$_.Subject -eq "CN=$CNclient"}) | Remove-Item -Force -ErrorAction SilentlyContinue
-Get-Item -Path "$ENV:USERPROFILE\Documents\$CNclient.pfx" | Remove-Item -Force -ErrorAction SilentlyContinue
